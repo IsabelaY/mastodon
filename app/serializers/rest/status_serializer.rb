d4dc3041757c8bd6044cb6ab5b57589f4b344305
QueerNeko <QueerNeko@users.noreply.github.com>
2018-09-17 01:34:49 -0300

    Adding ability to send local toot (federate as unlisted but show on local timeline as public)

--- app/serializers/rest/status_serializer.rb
+++ app/serializers/rest/status_serializer.rb
@@ -6,7 +6,7 @@ class REST::StatusSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :in_reply_to_id, :in_reply_to_account_id,
              :sensitive, :spoiler_text, :visibility, :language,
              :uri, :url, :replies_count, :reblogs_count,
-             :favourites_count, :edited_at, :local_only, :activity_pub_type
+             :favourites_count, :edited_at, :local_only, :public_in_local, :activity_pub_type
 
   attribute :favourited, if: :current_user?
   attribute :reblogged, if: :current_user?
@@ -83,6 +83,10 @@ class REST::StatusSerializer < ActiveModel::Serializer
 
   def url
     ActivityPub::TagManager.instance.url_for(object)
+  end
+  
+  def public_in_local
+    object.public_in_local == true
   end
 
   def favourited
