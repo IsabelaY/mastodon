27dc11bd43c7e427f6cadabc691f9b5403bb0c04
QueerNeko <QueerNeko@users.noreply.github.com>
2019-01-06 20:24:17 -0200

    Adding setting for emoji size

--- app/controllers/settings/preferences_controller.rb
+++ app/controllers/settings/preferences_controller.rb
@@ -58,6 +58,9 @@ class Settings::PreferencesController < Settings::BaseController
       :setting_crop_images,
       :setting_always_send_emails,
       :setting_expand_usernames,
+      :setting_emoji_size_simple,
+      :setting_emoji_size_detailed,
+      :setting_emoji_size_name,
       notification_emails: %i(follow follow_request reblog favourite mention report pending_account trending_tag appeal),
       interactions: %i(must_be_follower must_be_following must_be_following_dm)
     )
