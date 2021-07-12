f98a9b76c3b5104477bd8b65df9d464563a6596f
QueerNeko <QueerNeko@users.noreply.github.com>
2021-07-12 01:37:48 -0300

    Add max_toot_chars and max_bio_chars to instance API

--- app/serializers/rest/instance_serializer.rb
+++ app/serializers/rest/instance_serializer.rb
@@ -6,7 +6,8 @@ class REST::InstanceSerializer < ActiveModel::Serializer
   attributes :uri, :title, :short_description, :description, :email,
              :version, :urls, :stats, :thumbnail,
              :languages, :registrations, :approval_required, :invites_enabled,
-             :configuration
+             :configuration,
+             :max_toot_chars, :max_bio_chars,
 
   has_one :contact_account, serializer: REST::AccountSerializer
 
@@ -40,6 +41,14 @@ class REST::InstanceSerializer < ActiveModel::Serializer
 
   def thumbnail
     instance_presenter.thumbnail ? full_asset_url(instance_presenter.thumbnail.file.url) : full_pack_url('media/images/preview.jpg')
+  end
+
+  def max_toot_chars
+    2400
+  end
+
+  def max_bio_chars
+    800
   end
 
   def stats
