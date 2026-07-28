0334c9ae73f96a8400cf167746c9dc38c6e3c071
Aster S. <asters@tutanota.com>
2026-07-27 22:25:30 -0300

    more theme fixes

--- app/javascript/mastodon/components/logo.tsx
+++ app/javascript/mastodon/components/logo.tsx
@@ -1,19 +1,19 @@
-import logo from '@/images/logo.svg';
+import logo from '@/images/colorides-logo.svg';
 
 export const WordmarkLogo: React.FC = () => (
   <svg viewBox='0 0 261 66' className='logo logo--wordmark' role='img'>
-    <title>Mastodon</title>
+    <title>Colorid.es</title>
     <use xlinkHref='#logo-symbol-wordmark' />
   </svg>
 );
 
 export const IconLogo: React.FC = () => (
   <svg viewBox='0 0 79 79' className='logo logo--icon' role='img'>
-    <title>Mastodon</title>
+    <title>Colorid.es</title>
     <use xlinkHref='#logo-symbol-icon' />
   </svg>
 );
 
 export const SymbolLogo: React.FC = () => (
-  <img src={logo} alt='Mastodon' className='logo logo--icon' />
+  <img src={logo} alt='Colorid.es' className='logo logo--icon' />
 );
